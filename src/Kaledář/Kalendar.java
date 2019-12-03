25f2cbd21dda6bc884f07a3d7c55ed8664b882af
2019-e-morgan <oliver-parker.morgan@student.gyarab.cz>
2019-12-03 14:03:47 +0100

    fd

--- src/Kaledář/Kalendar.java
+++ src/Kaledář/Kalendar.java
@@ -2,17 +2,69 @@ package Kaledář;
 
 public class Kalendar {
     private int[] pocetdnuvmesici = {31,28,31,30,31,30,31,31,30,31,30,31};
+    private boolean prestupny;
 
-    public int Poctednuvroce(){
-        int sum = 0;
-        for(int s:pocetdnuvmesici){
-            sum+=s;
+    Kalendar(boolean prestupny){
+        this.prestupny = prestupny;
+        if(prestupny){
+            pocetdnuvmesici[1]++;
         }
-        return sum;
     }
 
+    private int poradiDne(int mesic, int den){
+        int poradi = den;
+        for(int i=0;i<mesic-1;i++){
+            poradi +=pocetdnuvmesici[i];
+        }
+        return poradi;
+    }
+    private int[] denVPoradi(int poradi){
+        int mesic;
+        for(mesic = 0;poradi-pocetdnuvmesici[mesic]>0;mesic++){
+            poradi-=pocetdnuvmesici[mesic];
+        }
+
+        return new int[]{mesic+1,poradi};
+    }
+
+    public int interval(int mesicOd, int denOd, int mesicDo, int denDo){
+        return poradiDne(mesicOd,denOd)-poradiDne(mesicDo,denDo);
+    }
     public static void main(String[] args) {
-        Kalendar k = new Kalendar();
-        System.out.println(k.Poctednuvroce());
+        Kalendar k = new Kalendar(true);
+        System.out.println(k.poradiDne(12,31));
+        int[] a = k.denVPoradi(365);
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println();
+        System.out.println(k.interval(1,0,1,0));
+    }
+
+    public int[] getPocetdnuvmesici() {
+        return pocetdnuvmesici;
+    }
+
+    public boolean isPrestupny() {
+        return prestupny;
+    }
+
+    public void setPocetdnuvmesici(int[] pocetdnuvmesici) {
+        if(pocetdnuvmesici.length!=12){
+            System.out.println("Error: array has to have twelve elements");
+        }else {
+            this.pocetdnuvmesici = pocetdnuvmesici;
+        }
+    }
+
+    public void setPrestupny(boolean prestupny) {
+        if(!prestupny && this.prestupny) {
+            pocetdnuvmesici[1]--;
+        }else if(prestupny && !this.prestupny){
+            pocetdnuvmesici[1]++;
+        }
+        this.prestupny = prestupny;
+
+
+
     }
 }
